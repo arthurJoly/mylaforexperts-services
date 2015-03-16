b77f7a53ee4ad5d6838af7dd9da965ed3d0b3824
arthurJoly <arthurjoly11@gmail.com>
2015-03-16 15:34:39 +0100

    test sending notification when creating question

--- services/profile/question.js
+++ services/profile/question.js
@@ -23,7 +23,7 @@ module.exports.createQuestion = function(request,response) {
 			}
 			else{
 				var message = new gcm.Message();
-				message.addData('key1', 'new question');
+				/*message.addData('key1', 'new question');
 				var regIds = ['APA91bFWeAuEPexPz_JNnFEy1wgOpMsFcY9Pm8CRC1QNkA9Qz3QK05N01vAXLvtCS6Ofub2K0xAJIoMIF3tAOf5vAfP40wK4sKik6oPViJcjKy3tL6QfDhPvi2tDujFPvjKiIsZEGTXxtd8PD8WhOi0h7CVnjpxGF_dADW1Vz17iFj88eiB8AyU'];
 				var sender = new gcm.Sender('AIzaSyDz1bKCtAVnRYzUebc8AO-35uyqv7Wpu48');
 				sender.send(message, regIds, function (err, result) {
@@ -31,7 +31,7 @@ module.exports.createQuestion = function(request,response) {
 						console.error(err);
 					else    
 						console.log(result);
-				});
+				});*/
 				utils.httpResponse(response,200,'Question successfully created')
 			}
 		});
