4576deeb586a9712263ac6e54e944f90b4e0d7c2
ajoly <arthur.joly@mythalesgroup.com>
2015-04-30 16:26:44 +0200

    change sampel filter

--- services/profile/sample.js
+++ services/profile/sample.js
@@ -84,8 +84,8 @@ module.exports.sampleSearch = function(request,response) {
 			utils.httpResponse(response,404,err)
 		}else{
 			function filterSample(sample){
-				return (sample.specimenType.toString().equals(request.query.specimenType)
-				|| sample.environmentType.toString().equals(request.query.environmentType)
+				return (sample.specimenType.equals(request.query.specimenType)
+				|| sample.environmentType.equals(request.query.environmentType)
 				|| sample._id.toString().toLowerCase().startsWith(request.query.query.toLowerCase()));				
 			}
 			var samplesFiltered = samples.filter(filterSample);
