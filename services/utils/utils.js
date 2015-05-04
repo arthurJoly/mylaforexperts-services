b7d61cf334a360d9e40718dd8484008628f4b7f7
ajoly <arthur.joly@mythalesgroup.com>
2015-05-04 11:54:26 +0200

    rest services can be called by ajax

--- services/utils/utils.js
+++ services/utils/utils.js
@@ -28,7 +28,7 @@ function httpResponse(ajaxCallback, response,code,description,content) {
 		log.error(s + description)
 	}
 	
-	response.writeHead(code, { 'Content-Type': 'application/json',  'Access-Control-Allow-Origin': '*'});
+	response.writeHead(code, { 'Content-Type': 'application/json', 'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS', 'Access-Control-Allow-Origin': '*'});
 	
 	if(!ajaxCallback){
 		if(content === undefined){
